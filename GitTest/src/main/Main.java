6a9c0f7ee16c6f2e98c8e674dd15fd725cc3c0aa
Jaraujo <jorge.arj@gmail.com>
2013-12-18 21:48:15 +0000

    First commit

---
+++ GitTest/src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("This is a test aplication of Git functionalty");
+
+	}
+}
