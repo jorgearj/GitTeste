4b44cb49cbe56461fe14182f0e28c760f828e733
Jaraujo <jorge.arj@gmail.com>
2013-12-18 22:21:55 +0000

    new test

--- GitTest/src/main/Main.java
+++ GitTest/src/main/Main.java
@@ -7,6 +7,9 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		System.out.println("This is a test aplication of Git functionalty");
+		
+		System.out.println("e esta hein!!!");
+		//ai credo
 
 	}
 }
